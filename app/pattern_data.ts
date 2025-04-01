6e78b563c103b9e9ba33351488adc03c51ba4135
Evelyn <evelynzou@gmail.com>
2025-04-01 12:57:34 -0400

    added the real patterns

--- app/pattern_data.ts
+++ app/pattern_data.ts
@@ -11,6 +11,44 @@ const imgHW = 155;
 
 export const patterns: Pattern[] = [{
     id: "0",
+    name: "Pattern1",
+    imageUrl: "https://raw.githubusercontent.com/zouevelyn/ekua/refs/heads/main/app/patterns/Patern1.svg",
+    imageWidth: imgHW,
+    imageHeight: imgHW
+}, {
+    id: "1",
+    name: "Pattern2",
+    imageUrl: "https://raw.githubusercontent.com/zouevelyn/ekua/refs/heads/main/app/patterns/Pattern2.svg",
+    imageWidth: imgHW,
+    imageHeight: imgHW
+}, {
+    id: "2",
+    name: "Pattern3",
+    imageUrl: "https://raw.githubusercontent.com/zouevelyn/ekua/refs/heads/main/app/patterns/Pattern3.svg",
+    imageWidth: imgHW,
+    imageHeight: imgHW
+}, {
+    id: "3",
+    name: "Pattern4",
+    imageUrl: "https://raw.githubusercontent.com/zouevelyn/ekua/refs/heads/main/app/patterns/Pattern4.svg",
+    imageWidth: imgHW,
+    imageHeight: imgHW
+}, {
+    id: "4",
+    name: "Pattern5",
+    imageUrl: "https://raw.githubusercontent.com/zouevelyn/ekua/refs/heads/main/app/patterns/Pattern5.svg",
+    imageWidth: imgHW,
+    imageHeight: imgHW
+}, {
+    id: "5",
+    name: "Pattern6",
+    imageUrl: "https://raw.githubusercontent.com/zouevelyn/ekua/refs/heads/main/app/patterns/Pattern6.svg",
+    imageWidth: imgHW,
+    imageHeight: imgHW
+}]
+
+export const testPatterns: Pattern[] = [{
+    id: "0",
     name: "color-test1-055.svg (joy's test piece)",
     imageUrl: "https://raw.githubusercontent.com/zouevelyn/ekua/refs/heads/main/app/color-test/color-test1-05.svg",
     imageWidth: imgHW,
