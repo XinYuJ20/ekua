3f4375ac55862a124f9fff618897f183e60e4f1f
XinYuJ20 <xyjiang20@gmail.com>
2025-04-17 13:47:21 -0400

    updated colors

--- app/patterns/[id]/page.tsx
+++ app/patterns/[id]/page.tsx
@@ -86,7 +86,7 @@ export default function ColorPage( { params }: { params: Promise<{ id: string }>
                     selectedColor: null,
                     chosenColor: '#D60032',
                     fillSpeed: 0.15,
-                    colors: ['#D60032', '#FFFFFF', '#FFE208', '#C20ADD', '#00D420', '#2EFFEF', '#FF6700', '#283CEA'],
+                    colors: ['  #EA005F', '#FF7D26', '#FFDA00', '#4ADD3A', '#1935D8', '#C20ADD'],
                     closeOffset: null,
                     imgURL: null,
                     coloredURL: null
@@ -193,7 +193,7 @@ export default function ColorPage( { params }: { params: Promise<{ id: string }>
                     g.swatchHolder.style.flexWrap = 'wrap';  // Allows wrapping if needed
                     g.swatchHolder.style.justifyContent = 'center'; // Centers the swatches
                     g.swatchHolder.style.alignItems = 'center';
-                    g.swatchHolder.style.padding = '15px';
+                    // g.swatchHolder.style.padding = '15px';
                     g.swatchHolder.style.width = '100%';  // Adjust width dynamically
                     g.swatchHolder.style.maxWidth = '100%'; // Prevents overflow
                     g.swatchHolder.style.borderRadius = '20px';
@@ -219,7 +219,7 @@ export default function ColorPage( { params }: { params: Promise<{ id: string }>
                         swatch.style.backgroundColor = color;
                         swatch.dataset.color = color;
                         swatch.style.height = '50px';
-                        swatch.style.width = '25%';
+                        swatch.style.width = '33%';
                         swatch.style.paddingBottom = '25%';
                         swatch.style.margin = '0px';
                         swatch.style.display = 'inline-block';
